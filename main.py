5ed1b2385f41cbdeb48eddc605cc7bf90d4a0ca0
Novke <novke.brate@gmail.com>
2025-03-09 22:16:36 +0100

    unapredjen json example

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ from jsonextract import *
 from basicgpt import ask_gpt
 import json
 
-PDF_PATH = "input/CV.pdf"
-TMPL_PATH = "input/example1.json"
+PDF_PATH = "input/CVjusufML.docx"
+TMPL_PATH = "jsonexamples/example3.json"
 API_KEY = os.getenv("APIKEY_XENON_OPENAI")
-PROMPT = "You analyze PDF documents of CVs and detect specific details in them, and return them formatted in a json based on a template provided." 
+PROMPT = "You analyze PDF or DOC documents of CVs and detect specific details in them, and return them formatted in a json based on a template provided. Extract all the data you can find in the CV in the JSON example provided, and if there isnt such data for a certain field, just leave a simple '/'" 
 
 with open(TMPL_PATH, "r", encoding="utf-8") as file:
     TEMPLATE = json.load(file)
     
 QUESTION = "Template of the json is: " + json.dumps(TEMPLATE)
 
+def print_node(json1, node):
+    print("Node:", node)
+    print(get_json_value(json1, node))
+
 
 print("File analyze:")
 response1 = analyze_from_file(API_KEY, PDF_PATH, instructions=PROMPT, question=QUESTION)
 print(response1)
 json1 = extract_json(response1)
-print(get_json_value(json1, "personalInfo.name"))
-print(get_json_value(json1, "languages.1.proficiency"))
-print(get_json_value(json1, "technicalSkills.programmingLanguages.0"))
+print_node(json1, "personalInfo.name:")
+print_node(json1, "languages.1.proficiency")
+print_node(json1, "technicalSkills.programmingLanguages.0")
+print_node(json1, "certifications")
+print_node(json1, "hobbies.0")
+print_node(json1, "projects")
+print_node(json1, "projects.0.title")
 
 # print("Text analyze:")
 # TODO exception jer se u promptu salju "" koje treba escapovati
